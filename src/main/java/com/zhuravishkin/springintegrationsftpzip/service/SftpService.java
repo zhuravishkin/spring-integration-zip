26b524f238d9c51946d65bb09ee98c3d82d83fd4
alexey.zhuravishkin <zhuravishkin@gmail.com>
2021-04-20 17:17:50 +0300

    added folder creation

--- src/main/java/com/zhuravishkin/springintegrationsftpzip/service/SftpService.java
+++ src/main/java/com/zhuravishkin/springintegrationsftpzip/service/SftpService.java
@@ -1,6 +1,7 @@
 package com.zhuravishkin.springintegrationsftpzip.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.expression.common.LiteralExpression;
 import org.springframework.integration.sftp.session.SftpRemoteFileTemplate;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -21,18 +23,31 @@ public class SftpService {
 
     public void writeLiensToSftpFile() {
         List<String> strings = List.of("alpha", "beta", "gamma");
-        PipedInputStream pipedInputStream = new PipedInputStream();
-        try (PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream)) {
-            for (String s : strings) {
-                pipedOutputStream.write((s + "\n").getBytes(StandardCharsets.UTF_8));
+        try (PipedInputStream pipedInputStream = new PipedInputStream()) {
+            try (PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream)) {
+                for (String s : strings) {
+                    pipedOutputStream.write((s + "\n").getBytes(StandardCharsets.UTF_8));
+                }
+                pipedOutputStream.flush();
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
             }
-            pipedOutputStream.flush();
+            String path = "/2021/04/20";
+            sftpRemoteFileTemplate.setRemoteDirectoryExpression(new LiteralExpression(path));
+            sftpRemoteFileTemplate.execute(session -> {
+                StringBuilder builder = new StringBuilder(".sftp");
+                Arrays.stream(path.split("/")).forEach(s -> {
+                    try {
+                        session.mkdir(builder.append("/").append(s).toString());
+                    } catch (IOException e) {
+                        log.error(e.getMessage(), e);
+                    }
+                });
+                session.write(pipedInputStream, ".sftp/2021/04/20/test.log");
+                return null;
+            });
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
-        sftpRemoteFileTemplate.execute(session -> {
-            session.write(pipedInputStream, ".sftp/test.log");
-            return null;
-        });
     }
 }
